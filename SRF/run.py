bf0683c40d678effb6a99b92a06c03a3f4319b07
Sebkd <sebkd@mail.ru>
2022-09-25 17:58:40 +0300

    0.1.2 lesson_2

--- SRF/run.py
+++ SRF/run.py
@@ -1,13 +1,13 @@
+import pathlib
+import sys
 from wsgiref.simple_server import make_server
 
-from simple_rest_framework.main import Framework
-from urls import routes, fronts
-
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.resolve()))
+from SRF.simple_rest_framework.main import Framework
+from SRF.urls import routes, fronts
 
 application = Framework(routes, fronts)
 
 with make_server('', 8080, application) as httpd:
     print("Запуск на порту http://localhost:8080...")
     httpd.serve_forever()
-
-# [END FILE]
